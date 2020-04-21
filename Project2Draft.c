fd5837a59266746381f3141bcf935f5afd62df5f
unknown <rsm0015@auburn.edu>
2020-04-21 15:24:03 -0500

    Added internal LED definitions

--- Project2Draft.c
+++ Project2Draft.c
@@ -53,7 +53,7 @@
 #define GPIO_PORTF_RIS_R 		(*((volatile unsigned long *)0x40025414))
 
 // Define the clock
-#define SYSCTL_RCGC2_R      (*((volatile unsigned long *)0x400FE108))
+#define SYSCTL_RCGC2_R      	(*((volatile unsigned long *)0x400FE108))
 
 // Global Variables
 unsigned int state;
@@ -101,28 +101,34 @@ int main(void){
 	
 	while(1)
 	{
+		//waitForInterrupt();
 		//printf("Hi %0.0l \n",count); count++;
 		//tempVar = GPIO_PORTD_DATA_R;
 		getPhotoState();
 		if(state){
-			GPIO_PORTB_DATA_R |= 0x0F;
+			GPIO_PORTF_DATA_R &= ~(0x0A); 	// Turn off internal LEDs
+			GPIO_PORTF_DATA_R |= 0x02; 		// Turn on internal LEDs
+			GPIO_PORTB_DATA_R |= 0x0F;			// Turn on all external LEDs
 		}
 		else{
-			GPIO_PORTB_DATA_R &= ~0x0F;
+			GPIO_PORTF_DATA_R &= ~(0x0A); 	// Turn off internal red LED
+			GPIO_PORTF_DATA_R |= 0x08; 		// Turn on internal green LED
+			GPIO_PORTB_DATA_R &= ~0x0F;			// Turn off all external LEDs
 		}
 	}
 }
 
 void LEDCheck(void){	
-	// Set all three LEDs to be on, then blink three times;
+	// Make the LEDs blink three times
 	
 	GPIO_PORTB_DATA_R |= 0x0F; // Set (turn on PB0 PB1 PB2 PB3)
+	GPIO_PORTF_DATA_R &= ~0x0A; GPIO_PORTF_DATA_R |= 0x02; // Clear Internal, then set to red
 	Delay(1);
-	GPIO_PORTB_DATA_R ^= 0x0F; Delay(1); //GPIO_PORTB_DATA_R |= 0x0E; // Off and on
-	GPIO_PORTB_DATA_R ^= 0x0F; Delay(1); //GPIO_PORTB_DATA_R |= 0x0E; // Off and on
-	GPIO_PORTB_DATA_R ^= 0x0F; Delay(1); //GPIO_PORTB_DATA_R |= 0x0E; // Off and on
-	GPIO_PORTB_DATA_R ^= 0x0F; Delay(1);
-	GPIO_PORTB_DATA_R ^= 0x0F;     	     // Clear (turn off PB1 PB2 PB3)
+	GPIO_PORTB_DATA_R ^= 0x0F; GPIO_PORTF_DATA_R &= ~(0x02); GPIO_PORTF_DATA_R |= (0x08); Delay(1);
+	GPIO_PORTB_DATA_R ^= 0x0F; GPIO_PORTF_DATA_R &= ~(0x08); GPIO_PORTF_DATA_R |= (0x02); Delay(1);
+	GPIO_PORTB_DATA_R ^= 0x0F; GPIO_PORTF_DATA_R &= ~(0x02); GPIO_PORTF_DATA_R |= (0x08); Delay(1);
+	GPIO_PORTB_DATA_R ^= 0x0F; GPIO_PORTF_DATA_R &= ~(0x08); GPIO_PORTF_DATA_R |= (0x02); Delay(1);
+	GPIO_PORTB_DATA_R ^= 0x0F; GPIO_PORTF_DATA_R &= ~(0x02); // Clear (turn off PB1 PB2 PB3 PF1 PF3)
 }
 
 void PortB_Init(void)
@@ -151,27 +157,27 @@ void PortD_Init(void){
 	GPIO_PORTD_PCTL_R  &= ~(0xFF);  // 4) GPIO clear bit PCTL
 	GPIO_PORTD_DIR_R   &= ~(0xCF);	// 5) Set i/o: 0 = input  (PD0, PD1, PD2, PD3, PD6, PD7)
 	GPIO_PORTD_AFSEL_R &= ~(0xFF);	// 6) no alternate function
-	//GPIO_PORTD_PUR_R |= 0xF0;			// 7) Only enable pull-up resistor for negative logic switches
-	GPIO_PORTD_DEN_R   |=   0xFF;		// 8) Enable digital function (PD0 - 7)
+	//GPIO_PORTD_PUR_R |= 0xF0;		// 7) Only enable pull-up resistor for negative logic switches
+	GPIO_PORTD_DEN_R   |=   0xFF;	// 8) Enable digital function (PD0 - 7)
 }
 
 void PortF_Init(void)
 { 
   // Internal devices: 
   //     Switches: PF0, PF4 :: SW1, SW2
-  //     LED:      PF3 :: green LED
+  //     LED:      PF1, PF3 :: red LED, green LED
   volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;  // 1) activate F clock
-  delay = SYSCTL_RCGC2_R;        // delay
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;// 2) unlock PortF PF0  
-  GPIO_PORTF_CR_R = 0x1F;        // allow changes to PF4-0
-  GPIO_PORTF_AMSEL_R &= ~(0x19); // 3) disable analog function
-  GPIO_PORTF_PCTL_R  &= ~(0x19); // 4) GPIO clear bit PCTL
-  GPIO_PORTF_DIR_R   &= ~(0x11); // 5) Set i/o:  0 = input (PF0, PF4)
-  GPIO_PORTF_DIR_R   |=   0x08;  // 5) Set i/o:  1 = output (PF3)
-  GPIO_PORTF_AFSEL_R &= ~(0x19); // 6) no alternate function
-  GPIO_PORTF_PUR_R   |=   0x11;  // 7) Enable pull up on PF0, PF4
-  GPIO_PORTF_DEN_R   |=   0x19;  // 8) Enable digital function (PF0, PF3, PF4) 
+  SYSCTL_RCGC2_R |= 0x00000020;  	// 1) activate F clock
+  delay = SYSCTL_RCGC2_R;        	// delay
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;	// 2) unlock PortF PF0  
+  GPIO_PORTF_CR_R = 0x1F;        	// allow changes to PF4-0
+  GPIO_PORTF_AMSEL_R &= ~(0x19); 	// 3) disable analog function
+  GPIO_PORTF_PCTL_R  &= ~(0x19); 	// 4) GPIO clear bit PCTL
+  GPIO_PORTF_DIR_R   &= ~(0x11); 	// 5) Set i/o:  0 = input (PF0, PF4)
+  GPIO_PORTF_DIR_R   |=   0x0A;  	// 5) Set i/o:  1 = output (PF1, PF3)
+  GPIO_PORTF_AFSEL_R &= ~(0x19); 	// 6) no alternate function
+  GPIO_PORTF_PUR_R   |=   0x11;  	// 7) Enable pull up on PF0, PF4
+  GPIO_PORTF_DEN_R   |=   0x1B;  	// 8) Enable digital function (PF0, PF1, PF3, PF4) 
 }
 
 //Delay e * 100 ms
@@ -195,21 +201,35 @@ void getPhotoState(void)
   unsigned long In;
   In  = GPIO_PORTD_DATA_R;	// Collect phototransistor state
   In &= 0xF0; 				// Clear bits that we don't care about
-	//In = GPIO_PORTB_DATA_R;	// Collect and shift external switch states
-	if (In == 0x0F) 						      		// If all phototransistors are UNmasked
-	{
-		GPIO_PORTF_DATA_R |= (0x08);     		// Turn on green internal LED
-		//return;
-	}
-	else if (In == 0x00)									// If all phototransistors are masked
-	{
-		GPIO_PORTF_DATA_R |= (0x02);     		// Turn on red internal LED
-		//return;
-	}
-	else
-	{
-		GPIO_PORTF_DATA_R &= ~(0x0A);     	// Turn off both internal LEDs
-	}
-	state = In;
+  state = In;
 }
 
+void GPIOPortF_Handler(void){
+	// Do Nothing
+	/*
+	unsigned int In;
+	unsigned int flags_in;
+	GPIO_PORTF_DATA_R ^= (0x08);  //Toggle internal green LED
+	
+	flags_in = GPIO_PORTF_RIS_R; //save a copy of the interrupts
+	//flags_in = GPIO_PORTF_DATA_R;
+	if ((flags_in & 0x10) >> 4) //flag4 - PF4 triggered
+	{
+		GPIO_PORTF_ICR_R = 0x10;      // acknowledge flag4
+		FallingEdges = (FallingEdges + 1) & 0x0F; //Increment and truncate (wrap) toa 4-bit value
+	} else if (flags_in & 0x01) //flag0 - PF0 triggered
+	{
+		GPIO_PORTF_ICR_R = 0x01; 			// acknowledge flag0
+		FallingEdges = 0;             // clear count
+	}
+	//set LED's:
+	In = GPIO_PORTB_DATA_R;       //Save a copy of the portB data
+	In |= FallingEdges;           // Set   PB0 PB1 PB2 PB3 as appropriate.
+	In &= FallingEdges;           // Clear PB0 PB1 PB2 PB3 as appropriate.
+	GPIO_PORTB_DATA_R = In;       // Set port b data to light LED's
+	*/
+}
+
+void GPIOPortD_Handler(void){
+	// Do Nothing
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Embedded Systems Project
+
 ## Goal
 Develop a system capable of the following functionalities:
 1. When Switch SW2 is pushed, all external LEDs must be set off. The external red LED is the MOST significant bit. You must use the interrupt mechanism.
@@ -6,13 +7,16 @@ Develop a system capable of the following functionalities:
 3. When all phototransistors (including the sampling one)  are masked, the red LED on the Tiva must be on.
 4. When I mask any n sensing phototransistors, the external LEDs will count by n whenever I wave a finger over the sampling phototransistor. 
 5. If the previous functionalities are working, your surface area sensor is ready. Build a tray to repeatedly measure a 2cmx2cm dark square. Your sensor must repeatedly display the same reasonable count +/-1.
+
 See the assignment Project-Phase2.docx for more details
+
 ## How to Use
 Open Project2Draft.c and the accompanying startup.s in a Keil uVision project; compile and load to the Tiva. I re-used the C12_EdgeInterrupt directory and project because I received obscure errors when I tried to start a project from scratch.
 ## Current development state
-This version of the program reads from Port D but only determins if there is a signal from Port D or not. If a signal is found, all of the LEDs on Port B are illuminated. If a signal is not found, the LEDs are turned off.
+This version of the program reads from Port D but only determines if there is a signal from Port D or not. If a signal is found, all of the LEDs on Port B are illuminated. If a signal is not found, the LEDs are turned off. The internal LED is set to green if no signal is found and is red if a signal is found.
 Interrupts have been defined for Port F and Port D but are not yet implemented in the task code.
 ## To Do
 - Implement Port F Interrupts
 - Implement Port D Interrupt
 - Implement task code to interpret the data from Port D and light LEDs accordingly
+- Clean up the code for submission (remove LEDCheck and Delay functions)
